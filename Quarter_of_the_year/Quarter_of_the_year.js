223b844cd2a28bbc9ac331c16dd134a807d3fd65
Roberto Tapia <roberttapia7@gmail.com>
2022-03-16 12:23:30 -0700

    First Commit

---
+++ Quarter_of_the_year/Quarter_of_the_year.js
@@ -0,0 +1,11 @@
+const quarterOf = (month) => {
+    if (month/3 <= 1) {
+      return 1
+    } else if (month/3 <= 2) {
+      return 2
+    } else if (month/3 <= 3) {
+      return 3
+    } else {
+      return 4
+    }
+  }
