d969e325931d17e5e871bb0c4d953745324a278d
Roberto Tapia <tapiaroberto1994@gmail.com>
2022-06-09 22:44:59 -0700

    solution 27

---
+++ solution027.js
@@ -0,0 +1,4 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+const repeatStr = (n, s) => s.repeat(n)
