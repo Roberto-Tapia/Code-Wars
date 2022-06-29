407b9e7450d900810b19354c6408e2503fb0d7bb
Roberto Tapia <tapiaroberto1994@gmail.com>
2022-06-29 16:29:58 -0700

    solution 50

---
+++ solution050.js
@@ -0,0 +1,8 @@
+// Is it a palindrome?
+
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+function isPalindrome(x) {
+    let reversed = x.split('').reverse().join('')
+    return x.toLowerCase() === reversed.toLowerCase()
+  }
