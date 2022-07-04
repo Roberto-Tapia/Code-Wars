8624b5103fa944814376219c4d7624d7fc5d8a7c
Roberto Tapia <tapiaroberto1994@gmail.com>
2022-07-04 16:07:59 -0700

    solutiion 44

---
+++ solution044.js
@@ -0,0 +1,9 @@
+// Remove anchor from URL
+
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+    let array = url.split('#')
+    return array[0]
+  }
