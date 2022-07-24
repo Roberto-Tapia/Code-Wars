ac941a6829bf50e2fb3b6daa0dbe954c6f85fc81
Roberto Tapia <tapiaroberto1994@gmail.com>
2022-07-23 22:12:13 -0700

    solution 76

---
+++ solution076.js
@@ -0,0 +1,8 @@
+// Count of positives / sum of negatives
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+    return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+}
