aa388f96566ab29c953c6e10bf38606c6421e4c7
Roberto Tapia <tapiaroberto1994@gmail.com>
2022-08-03 20:22:23 -0700

    solution 64

---
+++ solution064.js
@@ -0,0 +1,11 @@
+// Grasshopper - Terminal game combat function
+
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+function combat(health, damage) {
+    if (health >= 0 && damage <= health){
+      return health - damage
+    } else {
+      return 0
+    }
+  }
