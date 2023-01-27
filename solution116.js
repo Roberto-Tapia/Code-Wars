4352a13a45b96e499939617437b13d28585674ed
Roberto Tapia <tapiaroberto1994@gmail.com>
2023-01-26 21:33:42 -0800

    solution116.js

---
+++ solution116.js
@@ -0,0 +1,11 @@
+// Factorial
+
+// Your task is to write function factorial.
+
+// function factorial(n){
+//     let result = 1
+//     for (let i = n; n > 0; n--){
+//       result *= n
+//     }
+//     return result
+//   }
