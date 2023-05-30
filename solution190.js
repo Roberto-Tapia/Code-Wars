45d676a22f1501330eed603180f3cb9141f6b04b
Roberto-Tapia <tapiaroberto1994@gmail.com>
2023-05-30 00:37:42 -0700

    solution190.js

---
+++ solution190.js
@@ -0,0 +1,9 @@
+// Grasshopper - Combine strings
+
+// Combine strings function
+
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+function combineNames(name1, name2) {
+    return name1 + " " + name2
+  }
