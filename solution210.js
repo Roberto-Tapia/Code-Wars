99b4022026f35942e5b73c7151ec4b0b1c04cd0d
Roberto-Tapia <tapiaroberto1994@gmail.com>
2023-06-28 21:23:36 -0700

    solution210.js

---
+++ solution210.js
@@ -0,0 +1,8 @@
+// Are arrow functions odd?
+
+// Time to test your basic knowledge in functions! Return the odds from a list:
+
+function odds(values){
+  // arrow it
+  return values.filter(x => x % 2 !== 0);
+}
