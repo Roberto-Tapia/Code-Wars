bd7f56b784e72df6aa52f25f6e1e1bc735734e2d
Roberto-Tapia <tapiaroberto1994@gmail.com>
2023-07-27 22:13:14 -0700

    solution218.js

---
+++ solution218.js
@@ -0,0 +1,23 @@
+// Sorted? yes? no? how?
+
+// Complete the method which accepts an array of integers, and returns one of the following:
+
+//     "yes, ascending" - if the numbers in the array are sorted in an ascending order
+//     "yes, descending" - if the numbers in the array are sorted in a descending order
+//     "no" - otherwise
+
+// You can assume the array will always be valid, and there will always be one correct answer.
+
+
+function isSortedAndHow(array) {
+    const ascending = [...array].sort((a, b) => a - b);
+    const descending = [...array].sort((a, b) => b - a);
+  
+    if (JSON.stringify(array) === JSON.stringify(ascending)) {
+      return 'yes, ascending';
+    } else if (JSON.stringify(array) === JSON.stringify(descending)) {
+      return 'yes, descending';
+    } else {
+      return 'no';
+    }
+  }
