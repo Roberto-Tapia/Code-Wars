690c6cc59204590d7c9dc3b50403e30eba079251
Roberto-Tapia <tapiaroberto1994@gmail.com>
2023-08-24 08:38:39 -0700

    solution232.js

---
+++ solution232.js
@@ -0,0 +1,13 @@
+// What is between?
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+function between(a, b) {
+    let solution = []
+    
+    for (let i = a; i <= b; i++){
+      solution.push(i)
+    }
+    
+    return solution
+  }
